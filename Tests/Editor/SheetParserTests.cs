1dcfdfcc04817f92695d33da2138a284c2af52e7
Wen Taichi <daleth90@gmail.com>
2023-04-09 20:56:23 +0800

    fix: fix tests

--- Tests/Editor/SheetParserTests.cs
+++ Tests/Editor/SheetParserTests.cs
@@ -71,13 +71,13 @@ namespace Physalia.ExcelDataExporter.Tests
         public void DataTable_BuiltInTypes()
         {
             var sheetRawData = new SheetRawData(3, 3);
-            sheetRawData.SetRow(0, "Field1", "Field2", "Field3");
+            sheetRawData.SetRow(0, "field1", "field2", "field3");
             sheetRawData.SetRow(1, "int", "string", "bool");
             sheetRawData.SetRow(2, "42", "abc", "true");
 
             var parser = new SheetParser();
 
-            string expected = "{\"Field1\":42,\"Field2\":\"abc\",\"Field3\":true}\n";
+            string expected = "{\"field1\":42,\"field2\":\"abc\",\"field3\":true}\n";
             string actual = parser.ExportDataTableAsJson(sheetRawData);
             Assert.AreEqual(expected, actual);
         }
@@ -86,13 +86,13 @@ namespace Physalia.ExcelDataExporter.Tests
         public void DataTable_BuiltInTypes_Array()
         {
             var sheetRawData = new SheetRawData(3, 2);
-            sheetRawData.SetRow(0, "Field1", "Field2");
+            sheetRawData.SetRow(0, "field1", "field2");
             sheetRawData.SetRow(1, "int[]", "bool[]");
             sheetRawData.SetRow(2, "1,2,3", "true,false,true");
 
             var parser = new SheetParser();
 
-            string expected = "{\"Field1\":[1,2,3],\"Field2\":[true,false,true]}\n";
+            string expected = "{\"field1\":[1,2,3],\"field2\":[true,false,true]}\n";
             string actual = parser.ExportDataTableAsJson(sheetRawData);
             Assert.AreEqual(expected, actual);
         }
