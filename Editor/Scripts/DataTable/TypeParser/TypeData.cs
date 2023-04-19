fbe9084e354b9f8e86e7dcac664299c3ff5562ce
Wen Taichi <daleth90@gmail.com>
2023-04-19 10:47:48 +0800

    refactor: remove deprecated method

--- Editor/Scripts/DataTable/TypeParser/TypeData.cs
+++ Editor/Scripts/DataTable/TypeParser/TypeData.cs
@@ -9,24 +9,5 @@ namespace Physalia.ExcelDataExporter
         public List<FieldData> fieldDatas = new();
 
         public bool IsSystemType => TypeUtility.IsSystemType(name);
-
-        public bool Validate()
-        {
-            // Check if field name is duplicate
-            var fieldNames = new HashSet<string>();
-            for (var i = 0; i < fieldDatas.Count; i++)
-            {
-                FieldData fieldData = fieldDatas[i];
-                if (fieldNames.Contains(fieldData.name))
-                {
-                    Debug.LogError($"The type '{name}' already contains a definition for '{fieldData.name}'");
-                    return false;
-                }
-
-                fieldNames.Add(fieldData.name);
-            }
-
-            return true;
-        }
     }
 }
