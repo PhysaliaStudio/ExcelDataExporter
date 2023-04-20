8ffbc1d91e84583a88242fee73891a3e21f795cd
Wen Taichi <daleth90@gmail.com>
2023-04-20 16:43:31 +0800

    refactor: make exporter support SerializedObject

--- Editor/Scripts/DataTable/DataExporterScriptableObject.cs
+++ Editor/Scripts/DataTable/DataExporterScriptableObject.cs
@@ -9,14 +9,13 @@ namespace Physalia.ExcelDataExporter
         public ScriptableObject Export(TypeData typeData, SheetRawData sheetRawData)
         {
             Type tableType = typeData.GetTableType();
-            var dataTable = ScriptableObject.CreateInstance(tableType);
-
+            ScriptableObject dataTable = ScriptableObject.CreateInstance(tableType);
             var serializedObject = new SerializedObject(dataTable);
-            SerializedProperty property = serializedObject.FindProperty("items");
 
             string metadataText = sheetRawData.Get(Const.SheetMetaRow, Const.SheetMetaColumn);
             Metadata metadata = Metadata.Parse(metadataText);
 
+            SerializedProperty property = serializedObject.FindProperty("items");
             if (metadata.SheetLayout == SheetLayout.Horizontal)
             {
                 property.arraySize = sheetRawData.RowCount - Const.DataTableStartRow;
@@ -41,7 +40,7 @@ namespace Physalia.ExcelDataExporter
             return dataTable;
         }
 
-        private void ExportDataAsItem(SerializedProperty element, TypeData typeData, string[] dataRow)
+        private void ExportDataAsItem(SerializedObject @object, TypeData typeData, string[] dataRow)
         {
             var columnIndex = 0;
             for (var i = 0; i < typeData.fieldDatas.Count; i++)
@@ -49,26 +48,104 @@ namespace Physalia.ExcelDataExporter
                 FieldData fieldData = typeData.fieldDatas[i];
                 if (fieldData.IsSystemType)  // If the field is system type, write it directly
                 {
-                    WritePropertyForSystemType(element, fieldData, dataRow[columnIndex], true);
+                    WriteForSystemType(@object, fieldData, dataRow[columnIndex], true);
                     columnIndex++;
                 }
                 else  // If the field is custom type, write it recursively
                 {
-                    columnIndex = WritePropertyForCustomType(element, fieldData, dataRow, columnIndex, true);
+                    columnIndex = WriteForCustomType(@object, fieldData, dataRow, columnIndex, true);
                 }
             }
         }
 
-        private void WritePropertyForSystemType(SerializedProperty element, FieldData fieldData, string dataText, bool isRoot)
+        private void ExportDataAsItem(SerializedProperty property, TypeData typeData, string[] dataRow)
         {
-            string propertyName = isRoot ? fieldData.NameForPrivateField : fieldData.NameForPublicField;
-            SerializedProperty fieldProperty = element.FindPropertyRelative(propertyName);
+            var columnIndex = 0;
+            for (var i = 0; i < typeData.fieldDatas.Count; i++)
+            {
+                FieldData fieldData = typeData.fieldDatas[i];
+                if (fieldData.IsSystemType)  // If the field is system type, write it directly
+                {
+                    WriteForSystemType(property, fieldData, dataRow[columnIndex], true);
+                    columnIndex++;
+                }
+                else  // If the field is custom type, write it recursively
+                {
+                    columnIndex = WriteForCustomType(property, fieldData, dataRow, columnIndex, true);
+                }
+            }
+        }
+
+        private void WriteForSystemType(SerializedObject @object, FieldData fieldData, string dataText, bool isRoot)
+        {
+            SerializedProperty fieldProperty = FindFieldProperty(@object, fieldData, isRoot);
             if (fieldProperty == null)
             {
-                Debug.LogError($"Field '{propertyName}' not found!");
                 return;
             }
 
+            WritePropertyForSystemType(fieldProperty, fieldData, dataText);
+        }
+
+        private void WriteForSystemType(SerializedProperty property, FieldData fieldData, string dataText, bool isRoot)
+        {
+            SerializedProperty fieldProperty = FindFieldProperty(property, fieldData, isRoot);
+            if (fieldProperty == null)
+            {
+                return;
+            }
+
+            WritePropertyForSystemType(fieldProperty, fieldData, dataText);
+        }
+
+        private int WriteForCustomType(SerializedObject @object, FieldData fieldData, string[] dataRow, int columnIndex, bool isRoot)
+        {
+            SerializedProperty fieldProperty = FindFieldProperty(@object, fieldData, isRoot);
+            if (fieldProperty == null)
+            {
+                return columnIndex;
+            }
+
+            return WritePropertyForCustomType(fieldProperty, fieldData, dataRow, columnIndex);
+        }
+
+        private int WriteForCustomType(SerializedProperty property, FieldData fieldData, string[] dataRow, int columnIndex, bool isRoot)
+        {
+            SerializedProperty fieldProperty = FindFieldProperty(property, fieldData, isRoot);
+            if (fieldProperty == null)
+            {
+                return columnIndex;
+            }
+
+            return WritePropertyForCustomType(fieldProperty, fieldData, dataRow, columnIndex);
+        }
+
+        private static SerializedProperty FindFieldProperty(SerializedObject @object, FieldData fieldData, bool isRoot)
+        {
+            string propertyName = isRoot ? fieldData.NameForPrivateField : fieldData.NameForPublicField;
+            SerializedProperty fieldProperty = @object.FindProperty(propertyName);
+            if (fieldProperty == null)
+            {
+                Debug.LogError($"Field '{propertyName}' not found!");
+            }
+
+            return fieldProperty;
+        }
+
+        private static SerializedProperty FindFieldProperty(SerializedProperty property, FieldData fieldData, bool isRoot)
+        {
+            string propertyName = isRoot ? fieldData.NameForPrivateField : fieldData.NameForPublicField;
+            SerializedProperty fieldProperty = property.FindPropertyRelative(propertyName);
+            if (fieldProperty == null)
+            {
+                Debug.LogError($"Field '{propertyName}' not found!");
+            }
+
+            return fieldProperty;
+        }
+
+        private void WritePropertyForSystemType(SerializedProperty fieldProperty, FieldData fieldData, string dataText)
+        {
             if (fieldData.IsArray)
             {
                 string[] splits = dataText.Split(',');
@@ -85,6 +162,66 @@ namespace Physalia.ExcelDataExporter
                 string text = dataText;
                 WriteValueForSystemType(fieldProperty, fieldData.BaseTypeName, text);
             }
+        }
+
+        private int WritePropertyForCustomType(SerializedProperty fieldProperty, FieldData fieldData, string[] dataRow, int columnIndex)
+        {
+            var iterator = new TypeFieldIterator(fieldData);
+            SerializedProperty writtenProperty = fieldProperty;
+
+            while (true)
+            {
+                bool isCurrentCellApplicable = !fieldData.IsArray || dataRow[columnIndex] != Const.NotApplicable;
+
+                if (iterator.IsAtFirstMember())
+                {
+                    if (isCurrentCellApplicable)
+                    {
+                        if (fieldData.IsArray)
+                        {
+                            fieldProperty.arraySize++;
+                            writtenProperty = fieldProperty.GetArrayElementAtIndex(fieldProperty.arraySize - 1);
+                        }
+                    }
+                }
+
+                FieldData currentMemberFieldData = iterator.CurrentMember;
+                if (currentMemberFieldData.IsSystemType)
+                {
+                    if (isCurrentCellApplicable)
+                    {
+                        WriteForSystemType(writtenProperty, currentMemberFieldData, dataRow[columnIndex], false);
+                    }
+                    columnIndex++;
+                }
+                else
+                {
+                    if (isCurrentCellApplicable)
+                    {
+                        columnIndex = WriteForCustomType(writtenProperty, currentMemberFieldData, dataRow, columnIndex, false);
+                    }
+                    else
+                    {
+                        columnIndex += currentMemberFieldData.EvaluateColumnCount();
+                    }
+                }
+
+                // Increase array index if not finished
+                if (!iterator.IsAtLastMember())
+                {
+                    iterator.IncreaseMemberIndex();
+                }
+                else if (iterator.IsArray && iterator.ArrayIndex < iterator.FieldData.arraySize - 1)
+                {
+                    iterator.IncreaseArrayIndex();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return columnIndex;
         }
 
         private void WriteValueForSystemType(SerializedProperty fieldProperty, string typeName, string text)
@@ -117,74 +254,6 @@ namespace Physalia.ExcelDataExporter
                         break;
                     }
             }
-        }
-
-        private int WritePropertyForCustomType(SerializedProperty element, FieldData fieldData, string[] dataRow, int columnIndex, bool isRoot)
-        {
-            string propertyName = isRoot ? fieldData.NameForPrivateField : fieldData.NameForPublicField;
-            SerializedProperty fieldProperty = element.FindPropertyRelative(propertyName);
-            if (fieldProperty == null)
-            {
-                Debug.LogError($"Field '{propertyName}' not found!");
-                return columnIndex;
-            }
-
-            var iterator = new TypeFieldIterator(fieldData);
-            SerializedProperty writtenProperty = fieldProperty;
-
-            while (true)
-            {
-                bool isCurrentCellApplicable = !fieldData.IsArray || dataRow[columnIndex] != Const.NotApplicable;
-
-                if (iterator.IsAtFirstMember())
-                {
-                    if (isCurrentCellApplicable)
-                    {
-                        if (fieldData.IsArray)
-                        {
-                            fieldProperty.arraySize++;
-                            writtenProperty = fieldProperty.GetArrayElementAtIndex(fieldProperty.arraySize - 1);
-                        }
-                    }
-                }
-
-                FieldData currentMemberFieldData = iterator.CurrentMember;
-                if (currentMemberFieldData.IsSystemType)
-                {
-                    if (isCurrentCellApplicable)
-                    {
-                        WritePropertyForSystemType(writtenProperty, currentMemberFieldData, dataRow[columnIndex], false);
-                    }
-                    columnIndex++;
-                }
-                else
-                {
-                    if (isCurrentCellApplicable)
-                    {
-                        columnIndex = WritePropertyForCustomType(writtenProperty, currentMemberFieldData, dataRow, columnIndex, false);
-                    }
-                    else
-                    {
-                        columnIndex += currentMemberFieldData.EvaluateColumnCount();
-                    }
-                }
-
-                // Increase array index if not finished
-                if (!iterator.IsAtLastMember())
-                {
-                    iterator.IncreaseMemberIndex();
-                }
-                else if (iterator.IsArray && iterator.ArrayIndex < iterator.FieldData.arraySize - 1)
-                {
-                    iterator.IncreaseArrayIndex();
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            return columnIndex;
         }
     }
 }
